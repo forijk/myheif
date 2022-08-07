17ef928c7ecae7de245aae412543e7a391f85b5c
Sandro Amato <sdroamt0@gmail.com>
2022-08-07 16:05:01 +0200

    Add files via upload

--- js/myheic.js
+++ js/myheic.js
@@ -2,33 +2,44 @@
 // https://github.com/alexcorvi/heic2any/blob/master/docs/getting-started.md
 // https://imagekit.io/blog/lazy-loading-images-complete-guide/
 function myReplace(anImg) {
-  //console.log("start fetching and decoding: " + anImg.dataset.src);
+
   fetch(anImg.dataset.src)
-    .then( (res) => res.blob() )
+  
+  .then( // on fetch() promise success
+    (res) => res.blob() 
+  )
     
-    .then( (blob) => heic2any({
-                      blob,
-	                    toType: "image/jpeg",
-	                    quality: 0.92,
-                    }) )
-                    
-    .then((conversionResult) => {
+  .then( // on blob() promise success
+    (blob) => heic2any({ 
+                blob,
+                toType: "image/jpeg",
+                quality: 0.92,
+              })
+  )
+  
+  .then( // on heic2any() promise success
+    (conversionResult) => { 
       // conversionResult is a BLOB
       // of the PNG formatted image
 	    anImg.src = URL.createObjectURL(conversionResult);
 	    anImg.title = "Decoded from heic/heif\nClick to open 1/1 resolution of:\n" + anImg.dataset.src;
 	    anImg.onclick = function(e){ window.open(anImg.src, '_blank'); }
-	    
-	    console.log(".then - fetched and decoded: " + anImg.dataset.src);
-    })
-    
-    .catch((e) => {
+	    console.log("+++ .then - fetched and decoded: " + anImg.dataset.src);
+    }
+  )
+
+  .catch(
+    (e) => {
       // see error handling section
-      console.log(".catch - decoding error decoding heic/heif: " + anImg.dataset.src + "\n" + e);
-      anImg.src = "images/HeifHeicError.png";
-      anImg.title = "images/HeifHeicError.png";
-    })
-    
+      console.error("*** .catch - fetching/decoding error heic/heif: " + anImg.dataset.src);
+      console.error(e);
+      if (!anImg.src) { //there is not an original image...
+        anImg.src = "images/HeifHeicError.png";
+        anImg.title = "images/HeifHeicError.png";
+      }
+    }
+  )
+
     /*
     .finally(() => {
       console.log(".finally - decoded or error: " + anImg.dataset.src);
@@ -51,11 +62,12 @@ function getCanvasFromImage(image) {
 
 window.onload=function() {
   var allImages = document.getElementsByTagName('img');
-  for(var i = 0; i < allImages.length ; i++) {
 
-    //allImages[i].onload = function(e) { console.log("*** loaded: " + this.src); }
+  //before replace the img onload
+  /*  only to write after decoding start
+  for(var i = 0; i < allImages.length ; i++) {
+    // allImages[i].onload = function(e) { console.log("***+++ loaded: " + this.src); }
     
-/* only to write after decoding start
     allImages[i].onload = function(e) {
       //this.onload = null; // avoid to recall onload
       this.onload = function(anEv) { console.log("++++++ loaded after replacing..." + anEv);}
@@ -64,25 +76,36 @@ window.onload=function() {
       
       var myCanvas = getCanvasFromImage(this);
       var myCtx = myCanvas.getContext("2d");
-      myPX =  ~~(myCanvas.width / 30);
+      myPX =  (myCanvas.width / 30)>>0;
       //myCtx.font = "30px Arial";
       myCtx.font = myPX + "px Arial";
       myCtx.fillStyle = 'blue';
-      myCtx.fillText("Decoded from heif/heic", ~~(myCanvas.width / 40), ~~(myCanvas.height / 16));
+      myCtx.fillText("Decoded from heif/heic", (myCanvas.width / 40)>>0, (myCanvas.height / 16)>>0 );
       
-      // double change...
-      console.log("------ replacing img with canvas blob..." + this);
+      //// double change...
+      //console.log("------ replacing img with canvas blob..." + this);
       //this.src = myCanvas.toDataURL();
-      var myImg = this;
-      myCanvas.toBlob(
-        function(myBlob) {
-          URL.revokeObjectURL(myImg.src); // at this point is the URL.createObjectURL after decoding
-          myImg.src = URL.createObjectURL(myBlob); //this containt the new text drown
-        }
-      );
+      //var myImg = this;
+      //myCanvas.toBlob(
+      //  function(myBlob) {
+      //    URL.revokeObjectURL(myImg.src); // at this point is the URL.createObjectURL after decoding
+      //    myImg.src = URL.createObjectURL(myBlob); //this containt the new text drown
+      //  }
+      //);
+      console.log("------ replacing img with canvas... replacing: " + this );
+      //myCanvas.title = this.title;
+      //myCanvas.onclick = this.onclick;
+      this.parentNode.replaceChild(myCanvas, this);
+      URL.revokeObjectURL(this.src);  // at this point is the URL.createObjectURL after decoding
+      this.remove();
       
     }
-// only to write after decoding end */
+  } // for(var i = 0; i < allImages.length ; i++) {
+  // only to write after decoding end */
+    
+  for(var i = 0; i < allImages.length ; i++) {
+
+    //allImages[i].onload = function(e) { console.log("*** loaded: " + this.src); }
     
     if (allImages[i].dataset.src) { // if img element contains data-src parameter
       if ( (allImages[i].dataset.src.substr(-5,5).toLowerCase() == ".heic") || (allImages[i].dataset.src.substr(-5,5).toLowerCase() == ".heif")) {
@@ -91,7 +114,7 @@ window.onload=function() {
         //allImages[i].src = 'url_to_cat_image';
       }
       else {
-        //console.log("not heic/heif extension but used data-src... simply replacing src with data-src: " + allImages[i].dataset.src);
+        console.error("*** not heic/heif extension but used data-src... simply replacing src with data-src: " + allImages[i].dataset.src);
         allImages[i].src = allImages[i].dataset.src;
         allImages[i].title = allImages[i].dataset.src;
       }
@@ -106,8 +129,8 @@ window.onload=function() {
         }
         myReplace(allImages[i]);
       }
-    }
-  }
+    }  
+  } // for(var i = 0; i < allImages.length ; i++) {
 }
 /* */
 // ---------- heic/heif decoder stop ----------
