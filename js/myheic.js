dc42d7bd975a7f03c605ca77e535aed7c752dc5b
Sandro Amato <sdroamt0@gmail.com>
2022-08-21 13:08:26 +0200

    Add files via upload

--- js/myheic.js
+++ js/myheic.js
@@ -14,6 +14,7 @@ function myReplace(anImg) {
                 blob,
                 toType: "image/jpeg",
                 quality: 0.92,
+                signature: anImg.title,
               })
   )
   
